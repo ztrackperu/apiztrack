6a48c306a89fd6f411b21fa766fee551d60f67c4
Luis Marcelo <ztrack@zgroup.com.pe>
2023-12-14 11:18:15 -0500

    mejorando el aspecto del sitio web con Tailwind CSS

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    // desde el modelo enviamos a index.blade 
+    //{$post->excerpt , primera letra en mayuscula
+    // se le agrega get + Excerpt + Atribute
+    public function getExcerptAttribute()
+    {
+        // toma los 120 primeros caracteres del campo contenido
+        return substr($this->content,0,120);
+
+    }
+    //{$post->published_at} , primera letra en mayuscula de ambas separaciones
+    // se le agrega get + PublishedAt + Atribute
+    public function getPublishedAtAttribute()
+    {
+        // da fomato de fecha legible
+        return $this->created_at->format('d/m/Y');
+        
+    }
+
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,20 +3,28 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://cdn.tailwindcss.com"></script>
 
         <title>Laravel</title>
 
 
     </head>
-    <body class="antialiased">
-        @foreach ($posts as $post)
-        <div>
-            <h2>{{$post->title}} </h2>
+    <body class="bg-gray-100 text-gray-700">
+        <div class="container mx-auto px-4">
+            <div class="grid grid-cols-3 my-10">
+                @foreach ($posts as $post)
+                <div class ="bg-white hover:bg-blue-100 border border-gray-200 p-5">
+                    <h2 class="font-bold text-lg mb-4">{{$post->title}} </h2>
+                    <p class="text-xs">{{$post->excerpt}} </p>
+                    <p class="text-xs text-right">{{$post->published_at}} </p>
+                </div>                  
+                @endforeach
+            </div>   
+            <div class="mb-10">
+                {{$posts->links()}}
+            </div>     
+           
         </div>
-            
-        @endforeach
-        
-        {{$posts->links()}}
 
     </body>
 </html>

29129d8099b4844904e3b2af01818f9ff3729e03
Luis Marcelo <ztrack@zgroup.com.pe>
2023-12-14 09:23:02 -0500

    sitio web : estructura inicial

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //creamos datos falsos
+            'user_id'=>rand(1,10),
+            'title' =>$this->faker->sentence,
+            'slug'=>$this->faker->slug,
+            'content'=>$this->faker->text(1600),
+
+        ];
+    }
+}

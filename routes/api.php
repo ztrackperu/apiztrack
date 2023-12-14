8928e4954666962b2a56289ccdf4ba4bdc3302dd
Luis Marcelo <ztrack@zgroup.com.pe>
2023-12-14 16:10:08 -0500

    version 2

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('v1/posts', App\Http\Controllers\Api\V1\PostController::class)
+// asignamos alias
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
+
+Route::apiResource('v1/posts', PostV1::class)
 ->only(['index','show','destroy']);
+
+Route::apiResource('v2/posts', PostV2::class)
+->only(['index','show']);
